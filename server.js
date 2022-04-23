0972aa47f4ea3c2b98eaa80263fcbf02df8a394d
Juan <jgarcia1545@gmail.com>
2022-04-23 15:29:47 -0700

    Created the express server with the api routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+// Variable Definition
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
 
