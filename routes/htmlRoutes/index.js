0e982b0556eabf4d4ba915972161869d9f937382
Juan <jgarcia1545@gmail.com>
2022-04-23 15:32:23 -0700

    Calls the notes and index html files

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,13 @@
+// Variable Definition
 const path = require('path');
 const router = require('express').Router();
 
+// Return the notes.html file
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
+// Return the index.html file
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
